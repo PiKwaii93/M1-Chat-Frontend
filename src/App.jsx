2d2b578058b7ed2527bfc9b5a6c661a7c6aaf970
samuelaudic <samuelaudic@gmail.com>
2023-12-01 19:23:37 +0100

    merge back to front

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useState, useEffect } from 'react';
 import io from 'socket.io-client';
 import Message from './Components/Atoms/Message';
@@ -7,17 +6,14 @@ import Icon from './Components/Atoms/Icon';
 import Image from './Components/Atoms/Image';
 import Avatar from './Components/Atoms/Avatar';
 
-const socket = io('http://localhost:5000');
-=======
-import {BrowserRouter, Routes, Route} from "react-router-dom"
+// const socket = io('http://localhost:5000');
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from "./components/Home"
 import ChatPage from "./components/ChatPage";
 import socketIO from "socket.io-client"
->>>>>>> c9a20ad297192d6750a8f26e502135319acf90c0
 
 const socket = socketIO.connect("http://localhost:4000")
 function App() {
-<<<<<<< HEAD
   const [messages, setMessages] = useState([]);
   const [messageText, setMessageText] = useState('');
 
@@ -55,18 +51,18 @@ function App() {
       <Icon value="icon1" />
       <Avatar connected={isConnected} size='medium' url='' />
       <Image url="https://cdn-icons-png.flaticon.com/256/25/25231.png" alt="Image test" />
-=======
-  return (
-    <BrowserRouter>
+
+      <BrowserRouter>
         <div>
           <Routes>
-            <Route path="/" element={<Home socket={socket}/>}></Route>
-            <Route path="/chat" element={<ChatPage socket={socket}/>}></Route>
+            <Route path="/" element={<Home socket={socket} />}></Route>
+            <Route path="/chat" element={<ChatPage socket={socket} />}></Route>
           </Routes>
->>>>>>> c9a20ad297192d6750a8f26e502135319acf90c0
+        </div>
+      </BrowserRouter>
+
     </div>
-    </BrowserRouter>
-    
+
   );
 }
 
