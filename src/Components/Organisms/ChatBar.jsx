57e323df557ddf9c0307ecad7e0c4e37b9cc1e85
DFarau <doriane.farau@hetic.net>
2023-12-06 22:20:53 +0100

    add Page Forlder and reorganise

--- src/Components/Organisms/ChatBar.jsx
+++ src/Components/Organisms/ChatBar.jsx
@@ -1,36 +1,23 @@
-import React, { useState, useEffect } from 'react';
-import CurrentUser from './Molecules/CurrentUser/CurrentUser';
-import Navigation from './Molecules/Navigation/Navigation';
-import Search from './Atoms/Search/Search';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, {useState, useEffect} from 'react'
 
-const ChatBar = ({ socket }) => {
+const ChatBar = ({socket}) => {
     const [users, setUsers] = useState([])
 
-    useEffect(() => {
+    useEffect(()=> {
         socket.on("newUserResponse", data => setUsers(data))
     }, [socket, users])
 
-    const isConnected = true;
-    const username = 'Gérard MANVUSSA';
-
-    return (
-        <div className='chat__sidebar'>
-
-            <CurrentUser username={username} />
-
-            <Navigation />
-
-            <h2>Open Chat</h2>
-            <div>
-                <Search />
-                <h4 className='chat__header'>ACTIVE USERS</h4>
-                <div className='chat__users'>
-                    {users.map(user => <p key={user.socketID}>{user.userName}</p>)}
-                </div>
+  return (
+    <div className='chat__sidebar'>
+        <h2>Open Chat</h2>
+        <div>
+            <h4  className='chat__header'>ACTIVE USERS</h4>
+            <div className='chat__users'>
+                {users.map(user => <p key={user.socketID}>{user.userName}</p>)}
             </div>
         </div>
-    )
+  </div>
+  )
 }
 
 export default ChatBar
