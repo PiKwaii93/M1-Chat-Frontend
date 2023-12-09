10407b1b990314c790b34728f695a76c74f6aab7
DFarau <doriane.farau@hetic.net>
2023-12-09 12:48:36 +0100

    update icon component

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,8 @@ import Avatar from './Components/Atoms/Avatar/Avatar';
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from "./Components/Pages/Home"
 import ChatPage from "./Components/Pages/ChatPage";
+import SignUp from "./Components/Pages/SignUp";
+import ResetPassword from './Components/Pages/ResetPassword';
 
 import socketIO from "socket.io-client"
 import './App.css'
@@ -31,13 +33,14 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Real-Time Chat App</h1>
+      {/* <h1>Real-Time Chat App</h1> */}
       {/* <div className="messages">
         {messages.map((message, index) => (
           <Message key={index} username={message.username} text={message.text} />
         ))}
       </div> */}
-      <div className="input-box">
+
+      {/* <div className="input-box">
         <input
           type="text"
           value={messageText}
@@ -45,13 +48,15 @@ function App() {
           placeholder="Type your message..."
         />
         <button onClick={sendMessage}>Send</button>
-      </div>
+      </div> */}
 
       <BrowserRouter>
         <div>
           <Routes>
             <Route path="/" element={<Home socket={socket} />}></Route>
-            <Route path="/chat" element={<ChatPage socket={socket} />}></Route>
+            <Route path="/chat/:email" element={<ChatPage socket={socket} />}></Route>
+            <Route path="/signup" element={<SignUp socket={socket} />}></Route>
+            <Route path="/reset-password" element={<ResetPassword socket={socket} />}></Route>
           </Routes>
         </div>
       </BrowserRouter>  
