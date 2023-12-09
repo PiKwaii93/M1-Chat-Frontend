f53c48da923fb4c544aa6da68d7b6ea6780a746b
DFarau <doriane.farau@hetic.net>
2023-12-09 11:43:23 +0100

    update input component

--- src/Components/Atoms/Input/Input.jsx
+++ src/Components/Atoms/Input/Input.jsx
@@ -1,8 +1,28 @@
+import React from 'react';
 import './Input.css';
 
-const Input = ({ type, placeholder, isRequired }) => {
+const Input = ({ type, placeholder, isRequired, value, onChange, name }) => {
+  const handleInputChange = (e) => {
+    if (onChange) {
+      onChange({
+        target: {
+          name,
+          value: e.target.value,
+        },
+      });
+    }
+  };
+
   return (
-    <input type="text" className={`input input__${type}`} placeholder={placeholder} required={isRequired} />
+    <input
+      type={type}
+      className={`input input__${type}`}
+      placeholder={placeholder}
+      required={isRequired}
+      value={value}
+      onChange={handleInputChange}
+      name={name}
+    />
   );
 };
 
